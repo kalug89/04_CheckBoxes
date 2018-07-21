160d5229efa6880125f7598221891599f08cda7b
kalug89 <kaluzny_david@hotmail.com>
2018-07-21 15:10:32 +0200

    fix configuration

--- app/src/main/java/pl/dkaluzny/checkboxes/MainActivity.kt
+++ app/src/main/java/pl/dkaluzny/checkboxes/MainActivity.kt
@@ -1,12 +1,17 @@
 package pl.dkaluzny.checkboxes
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.util.Log
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+    fun onCheckBoxClicked(view: View){
+        Log.wtf("Checks", "Checkbox CLicked")
     }
 }
