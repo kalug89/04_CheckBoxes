d35be93183bcb8620f1958978d6bf9b2c6f4418b
kalug89 <kaluzny_david@hotmail.com>
2018-07-21 15:29:09 +0200

    Look checkbox is checked in Logcat

--- app/src/main/java/pl/dkaluzny/checkboxes/MainActivity.kt
+++ app/src/main/java/pl/dkaluzny/checkboxes/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.util.Log
 import android.view.View
+import android.widget.CheckBox
 
 class MainActivity : AppCompatActivity() {
 
@@ -11,7 +12,28 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
-    fun onCheckBoxClicked(view: View){
-        Log.wtf("Checks", "Checkbox CLicked")
+
+    fun onCheckBoxClicked(view: View) {
+        view as CheckBox // cast our view as a Checkbox object or type or View
+        var isChecked: Boolean = view.isChecked
+
+        when (view.id) {
+            R.id.checkVeggies -> if (isChecked) {
+                Log.wtf("Veggies", "IsChecked")
+            } else {
+                Log.wtf("Veggies", "NoChecked")
+            }
+            R.id.checkFruit -> if (isChecked) {
+                Log.wtf("Fruit", "IsChecked")
+            } else {
+                Log.wtf("Fruit", "NoChecked")
+            }
+            R.id.checkMeat -> if (isChecked) {
+                Log.wtf("Meat", "IsChecked")
+            } else {
+                Log.wtf("Meat", "NoChecked")
+            }
+        }
     }
 }
+//        Log.wtf("Checks", "Checkbox CLicked")
